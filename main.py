3d433620e4de7741c76c3104e304c327e8501f9b
AnnaTupota <125197669+AnnaTupota@users.noreply.github.com>
2023-03-08 15:59:09 +0600

    Update main.py

--- main.py
+++ main.py
@@ -5,33 +5,33 @@ from openpyxl import load_workbook
 
 def parse1():
     fn = 'лаба1.xlsx'
-    wb = load_workbook(fn)
+    wb = load_workbook(fn)#Функция load_workbook () принимает имя файла в качестве аргумента и возвращает объект рабочей книги, который представляет файл.
     ws = wb['данные']
-    description1 = []  # Информация оОООООО
+    description1 = []
     description2 = []
     description3 = []
     count=2
+    s='https://www.chitai-gorod.ru/catalog/collections/bestsell?page='#адрес страницы
     for i in range(1, count + 1):
-        url1 = 'https://www.chitai-gorod.ru/catalog/collections/bestsell?page=' + str(i)  # передаем необходимы URL адрес
+        url1 = s + str(i)  # передаем необходимы URL адрес
         page1 = requests.get(url1)  # отправляем запрос методом Get на данный адрес и получаем ответ в переменную
-        print(page1.status_code)  # смотрим ответ
+        #Метод GET указывает на то, что происходит попытка извлечь данные из определенного ресурса. Для того, чтобы выполнить запрос GET, используется requests.get().
+        print(page1.status_code)  # смотрим ответ, можно увидеть код состояния, который возвращается с сервера
         soup1 = BeautifulSoup(page1.text, "html.parser")  # передаем страницу в bs4
-
+# создадим объект или дерево синтаксического анализа. Этот объект принимает в качестве аргументов документ из Requests (содержимое ответа сервера), а затем анализирует его
+        #указав в кавычках как он нам поможет 'html.parcer':
         block1 = soup1.findAll('article', class_='product-card product-card product')  # находим контейнер с нужным классом
 
 
         for data in block1:  # проходим циклом по содержимому контейнера
             nazv = data.find(class_='product-title__head')
-            if nazv is not None:
-                description1.append(nazv.text)
-
             author = data.find(class_='product-title__author')
-            if author is not None:
-                description2.append(author.text)
-
             price = data.find(class_='product-price__value')
-            if price is not None:
+            if ((nazv and author and price) is not None):
+                description1.append(nazv.text)
+                description2.append(author.text)
                 description3.append(price.text)
+
 
     print(description1, description2, description3)
     ech = 1
@@ -50,8 +50,6 @@ def parse1():
     wb.save(fn)
     wb.close()
 
-
-if __name__ == '__main__':
-   parse1()
+parse1()
 
 
