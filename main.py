c584aa113fa0168df7965542b6e05a6d750655b8
AnnaTupota <125197669+AnnaTupota@users.noreply.github.com>
2023-03-20 21:26:35 +0600

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
-from bs4 import BeautifulSoup  # импортируем библиотеку BeautifulSoup
-import requests  # импортируем библиотеку requests
+from bs4 import BeautifulSoup  # импортируем библиотеку BeautifulSoup, библиотека для синтаксического разбора файлов HTML/XML, которая может 
+# преобразовать даже неправильную разметку в дерево синтаксического разбора.
+import requests  # импортируем библиотеку requests, позволяет очень легко отправлять HTTP/1.1 запросы. 
 from openpyxl import load_workbook
 
 
 def parse1():
     fn = 'лаба1.xlsx'
-    wb = load_workbook(fn)#Функция load_workbook () принимает имя файла в качестве аргумента и возвращает объект рабочей книги, который представляет файл.
-    ws = wb['данные']
+    wb = load_workbook(fn) # Функция load_workbook () принимает имя файла в качестве аргумента и возвращает объект рабочей книги, который представляет файл.
+    ws = wb['данные'] # Рабочий лист можно получить, используя его имя в качестве ключа экземпляра созданной книги Excel
     description1 = []
     description2 = []
     description3 = []
@@ -17,9 +18,9 @@ def parse1():
         page1 = requests.get(url1)  # отправляем запрос методом Get на данный адрес и получаем ответ в переменную
         #Метод GET указывает на то, что происходит попытка извлечь данные из определенного ресурса. Для того, чтобы выполнить запрос GET, используется requests.get().
         print(page1.status_code)  # смотрим ответ, можно увидеть код состояния, который возвращается с сервера
-        soup1 = BeautifulSoup(page1.text, "html.parser")  # передаем страницу в bs4
-# создадим объект или дерево синтаксического анализа. Этот объект принимает в качестве аргументов документ из Requests (содержимое ответа сервера), а затем анализирует его
-        #указав в кавычках как он нам поможет 'html.parcer':
+        soup1 = BeautifulSoup(page1.text, "html.parser")  # передаем страницу в bs4, разбираем документ и передаем в конструктор BeautifulSoup 
+        # Этот объект принимает в качестве аргументов документ из Requests (содержимое ответа сервера), а затем анализирует его
+        #указав в кавычках как он нам поможет 'html.parcer'
         block1 = soup1.findAll('article', class_='product-card product-card product')  # находим контейнер с нужным классом
 
 
@@ -35,8 +36,9 @@ def parse1():
 
     print(description1, description2, description3)
     ech = 1
-    for elem1, elem2, elem3 in zip(description1, description2, description3):
-        cell = ws.cell(1, ech)
+    for elem1, elem2, elem3 in zip(description1, description2, description3): # Функция zip() в Python создает итератор, который объединяет 
+        #элементы из нескольких источников данных.
+        cell = ws.cell(1, ech) # получить ячейку - команда cell, вписываем в ячейку первой строки 1 столбца
         cell.value = elem1
 
         cell = ws.cell(2, ech)
@@ -47,8 +49,8 @@ def parse1():
 
         ech += 1
 
-    wb.save(fn)
-    wb.close()
+    wb.save(fn)#сохраняю изменения в фаиле
+    wb.close()#закрываю фаил
 
 parse1()
 
